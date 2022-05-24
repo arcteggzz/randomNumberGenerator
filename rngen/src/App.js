04fa1f1e89e7776e85516b3470f06ea10ddddd95
Oghenetega Esedere <arcteggzz@gmail.com>
2022-05-24 16:36:26 +0100

    added and tried some solutions

--- rngen/src/App.js
+++ rngen/src/App.js
@@ -2,26 +2,23 @@ import './App.css';
 import Number from './Components/Number';
 
 function App() {
-  const genNumbers = () => {
-    const name = Math.floor(Math.random() * 100)
-    const red = Math.floor(Math.random() * 257)
-    const green = Math.floor(Math.random() * 257)
-    const blue = Math.floor(Math.random() * 257)
-    return (
-      <div className="">
-        <Number name={name} bgColour={`rgb(${red}, ${green}, ${blue})`}/>
-      </div>
-    )
-  }
-  
-  const functionCall = () => {
-    while (true){
-      setInterval(genNumbers, 2000)
-    }
+  const genNumbers = () => { 
+      while (true) {
+        const name = Math.floor(Math.random() * 100)
+        const red = Math.floor(Math.random() * 257)
+        const green = Math.floor(Math.random() * 257)
+        const blue = Math.floor(Math.random() * 257)
+        return ( 
+        <div>
+          <Number name={name} bgColour={`rgb(${red},${green},${blue})`} />
+        </div>
+        )
+      }
   }
 
   return (
     <div className="App">
+      {genNumbers()}
     </div>
   );
 }
